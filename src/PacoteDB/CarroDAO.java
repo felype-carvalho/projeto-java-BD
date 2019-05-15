9a46580d9ecaa1eb623937cefe11bd5430b0c672
felype-carvalho <felype.oliveira@etec.sp.gov.br>
2019-05-14 22:18:44 -0300

    .

--- src/PacoteDB/CarroDAO.java
+++ src/PacoteDB/CarroDAO.java
@@ -1,14 +1,92 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package PacoteDB;
 
-/**
- *
- * @author LABORATORIO_INFO
- */
+import java.sql.*;
+import java.util.*;
+
 public class CarroDAO {
+
+    private Connection con = null;
+
+    public CarroDAO(Connection con) {
+        setCon(con);
+    }
+
+    public Connection getCon() {
+        return con;
+    }
+
+    public void setCon(Connection con) {
+        this.con = con;
+    }
+
+    public String inserir(CarroBean carro) {
+        String sql = "insert into carro(placa,cor,descricao)value(?,?,?)";
+
+        try {
+
+            PreparedStatement ps = getCon().prepareStatement(sql);
+
+            ps.setString(1, carro.getPlaca());
+            ps.setString(2, carro.getCor());
+            ps.setString(3, carro.getDescricao());
+
+            if (ps.executeUpdate() > 0) {
+                return "Inserido com sucesso!!!";
+            } else {
+                return "Erro ao inserir!!!";
+            }
+
+        } catch (SQLException e) {
+            return e.getMessage();
+        }
+    }
     
+    public String alterar(CarroBean carro) {
+        String sql = "update carro set cor=?, descricao=? where placa=?";
+
+        try {
+
+            PreparedStatement ps = getCon().prepareStatement(sql);
+
+            ps.setString(1, carro.getCor());
+            ps.setString(2, carro.getDescricao());
+            ps.setString(3, carro.getPlaca());
+
+            if (ps.executeUpdate() > 0) {
+                return "Inserido com sucesso!!!";
+            } else {
+                return "Erro ao alterar!!!";
+            }
+
+        } catch (SQLException e) {
+            return e.getMessage();
+        }
+    }
+    
+    public String deletar(CarroBean carro) {
+        String sql = "delete from carro where placa=?";
+
+        try {
+
+            PreparedStatement ps = getCon().prepareStatement(sql);
+
+            ps.setString(1, carro.getPlaca());
+
+            if (ps.executeUpdate() > 0) {
+                return "Inserido com sucesso!!!";
+            } else {
+                return "Erro ao alterar!!!";
+            }
+
+        } catch (SQLException e) {
+            return e.getMessage();
+        }
+    }
+    
+    public List<CarroBean> listaCarros(){
+        String sql = "select * from carro";
+        
+        
+    }
+
 }
--- src/PacoteDB/Conexao.java
+++ src/PacoteDB/Conexao.java
@@ -1,8 +1,40 @@
-
 package PacoteDB;
 
 import java.sql.*;
 
 public class Conexao {
-    
+
+    public static Connection abrirConexao() {
+        Connection con = null;
+
+        try {
+
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+
+            String url = "jdbc:mysql://127.0.0.1/estacionamento?"
+                    + "user=root&password=";
+
+            con = DriverManager.getConnection(url);
+
+            System.out.println("Conexão aberta!!");
+
+        } catch (SQLException e) {
+            System.out.println();
+        } catch (ClassNotFoundException e) {
+            System.out.println();
+        } catch (Exception e) {
+            System.out.println();
+        }
+        return con;
+    }
+
+    public void fecharConexao(Connection con) {
+        try {
+            con.close();
+        } catch (SQLException e) {
+
+        } catch (Exception e) {
+
+        }
+    }
 }
