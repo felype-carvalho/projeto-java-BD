bd98dd15346ff8c2518885959f5a77941bca4b7d
Davi de Oliveira <davi.santos123@etec.sp.gov.br>
2019-05-28 21:25:14 -0300

    classe java foi atualizada/refatorada

--- src/projetodb/Teste.java
+++ src/projetodb/Teste.java
@@ -4,16 +4,35 @@ import java.sql.*;
 import java.util.*;
 
 public class Teste {
+
     public static void main(String[] args) {
         Connection con = Conexao.openConnection();
         CarroBean cb = new CarroBean();
         CarroDAO cd = new CarroDAO(con);
-        
-        cb.setPlaca("JJJ9999");
-        cb.setCor("Amarelo");
-        cb.setDescricao("Carro SEMI-NOVO");
+
+        cb.setPlaca("KKK0000");
+        cb.setCor("Azul");
+        cb.setDescricao("Carro Zero");
         System.out.println(cd.inserir(cb));
+
+        //Testando método alterar
+        /*cb.setPlaca("JKL2897");
+        cb.setCor("Amarelo");
+        cb.setDescricao("Carro 3");
+        System.out.println(cd.alterar(cb));*/
+        //Testando excluir
+        /* cb.setPlaca("JKL2897");
+        System.out.println(cd.excluir(cb));*/
         
-     Conexao.closeConection(con);   
+        List<CarroBean> lista = cd.listarTodos();
+        if (lista != null) {
+            for (CarroBean carro : lista) {
+                System.out.println("Placa: " + carro.getPlaca());
+                System.out.println("Cor: " + carro.getCor());
+                System.out.println("Descrição:" + carro.getDescricao());
+            }
+        }
+
+        Conexao.closeConection(con);
     }
 }
